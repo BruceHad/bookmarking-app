b61c838a60bdd804a70fe79e3962af96738be0b3
Bruce <bahadden@gmail.com>
2017-12-22 12:17:09 +0000

    Forgot to add CORS back in

--- app.js
+++ app.js
@@ -7,8 +7,22 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 
-
 var app = express();
+
+// Support CORS
+var allowCrossDomain = function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,POST');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
+  // intercept OPTIONS method
+  if ('OPTIONS' == req.method) {
+    res.send(200);
+  }
+  else {
+    next();
+  }
+};
 
 
 // view engine setup
@@ -22,6 +36,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(allowCrossDomain);
 
 app.use('/', index);
 app.use('/index', index);
